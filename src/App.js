4f55294419322f61ffbf7a6d52b51a6bbfd8901f
Lex Zavala <lex.zavala7@gmail.com>
2021-05-30 22:40:38 -0500

    Added function to store the to do list using local storage and JSON

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
+import uuidv4 from 'uuid/dist/v4';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
-  const [todos, setTodos] = useState([])
-    const todoNameRef = useRef()
+    const [todos, setTodos] = useState([])
+    const todoNameRef = useRef();
 
+    // THIS EFFECT IS ALLOWING US TO LOAD OUR STORED JSON TODOS IF ANY
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (storedTodos) setTodos(storedTodos)
+    }, []);
+
+    // THIS EFFECT IS STORING THE TODOS TO BE PULLED NEXT TIME THE PAGE LOADS
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos]);
+
+    //THIS FUNCTION MAKING THE ADD BUTTON USE THE INPUT TO CREATE NEW TO DO OBJECTS
     function handleAddTodo (e){
        const name = todoNameRef.current.value;
-        if (name === "") return
-        console.log(name)
+        if (name === "") return;
+        setTodos(prevTodos => {
+            return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+        })
+        todoNameRef.current.value = null
     }
 
   return (
