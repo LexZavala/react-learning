14a31396a33c6d303a592e7fe28fc8324bfafa6c
Lex Zavala <lex.zavala7@gmail.com>
2021-05-31 21:29:02 -0500

    created CSS file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
 import uuidv4 from 'uuid/dist/v4';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos';
@@ -47,8 +49,21 @@ function App() {
 
   return (
       <>
-      <div class="d-flex justify-content-center">
+      <div class="d-flex justify-content-center" id="title">
         <h1>2 DO LIST</h1>
+      </div>
+      <div>
+          <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+          </Card>
       </div>
         <TodoList todos = {todos} toggleTodo = {toggleTodo} />
         <input ref={todoNameRef} type="text"/>
